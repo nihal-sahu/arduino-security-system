5d2b086a2844d84e5c1569ce9706a0d5bc9d4d96
Nihal Sahu <nihalsahu360@gmail.com>
2022-01-29 13:55:09 -0700

    added test code from following source: https://roboticsbackend.com/control-arduino-with-python-and-pyfirmata-from-raspberry-pi

---
+++ pi-arduino_testing/led_blink.py
@@ -0,0 +1,16 @@
+#THE FOLLOWING TEST CODE IS FROM https://roboticsbackend.com/control-arduino-with-python-and-pyfirmata-from-raspberry-pi/
+#!/usr/bin/env python3
+
+import pyfirmata
+import time
+
+if __name__ == '__main__':
+    board = pyfirmata.Arduino('/dev/ttyUSB0')
+    print("Communication Successfully started")
+    
+    while True:
+        board.digital[13].write(1)
+        time.sleep(1)
+        board.digital[13].write(0)
+        time.sleep(1)
+    
