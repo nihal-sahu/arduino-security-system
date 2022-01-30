a781d52d7fe58bd0f768cb46ccb527e2f34d2f4c
Nihal Sahu <87585163+nihal-sahu@users.noreply.github.com>
2022-01-29 23:38:10 -0700

    Update mail_protocol.py

--- mail_protocol.py
+++ mail_protocol.py
@@ -2,7 +2,7 @@ import time
 import smtplib
 import ssl
 
-def alert_email(receiver = "nihalsahu2003@gmail.com"):
+def alert_email(receiver = "youremail@gmail.com"):
     # receiver needs to be passed in the beginning but we can pass a default parameter for now 
     port = 465 
     smtp_server = "smtp.gmail.com"
