094eb8ee5ec1cb43e1d5222017be3b6800ff2cbd
Nihal Sahu <nihalsahu360@gmail.com>
2022-01-29 17:52:36 -0700

    added code for HC-SR04 ultrasonic ranging module to detect distances

--- sensor_read.py
+++ sensor_read.py
@@ -1,12 +1,28 @@
-#!/usr/bin/env python3
-
-import pyfirmata
+import sys
 import time
+from pymata4 import pymata4
 
-if __name__ == '__main__':
-    arduino = pyfirmata.Arduino('/dev/ttyUSB0')
-    print("Communication Successfully started")
-    
-    #same as void loop();
+board = pymata4.Pymata4()
+triggerPin = 10
+echoPin = 11
+DISTANCE_CM = 2
+
+# A callback function to display the distance
+def display(data):
+    """
+    The callback function to display the change in distance
+    :param data: [pin_type=12, trigger pin number, distance, timestamp]
+    """
+    print(f'Distance in cm: {data[DISTANCE_CM]}')
+
+def get_distance(callback):
+
+    # set the pin mode for the trigger and echo pins
+    board.set_pin_mode_sonar(triggerPin, echoPin, callback)
+    # wait forever
     while True:
-        
+        time.sleep(.01)
+        print(f'data read: {board.sonar_read(triggerPin)}')
+
+get_distance(display)
+
