3fdae3bf63b54c1724e9c13773f8f47c0d2e3e48
Nihal Sahu <nihalsahu360@gmail.com>
2022-01-29 18:57:32 -0700

    updated sensor_read.py

--- sensor_read.py
+++ sensor_read.py
@@ -1,24 +1,43 @@
 import sys
 import time
 from pymata4 import pymata4
+import mail_protocol
 
 board = pymata4.Pymata4()
 triggerPin = 10
 echoPin = 11
-DISTANCE_CM = 2
 
-# A callback function to display the distance
-def display(data):
-    print(f'Distance in cm: {data[DISTANCE_CM]}')
+def get_distance():
+    board.set_pin_mode_sonar(triggerPin, echoPin)
+    max_distance = 0;
+    startTime = time.time()
+    emailNotif = True
 
-def get_distance(callback):
-    board.set_pin_mode_sonar(triggerPin, echoPin, callback)
     #loops forever
     while True:
-        time.sleep(.01)
-        print(f'data read: {board.sonar_read(triggerPin)}')
+        time.sleep(0.2)
+        print(board.sonar_read(triggerPin)[0], "cm")
+        currentTime = time.time()
 
-print("The pymata4 library can be found at https://github.com/MrYsLab/pymata4\nThe following code was developed by Nihal Sahu and Nishan Sivakumar\n")
+        #condition only executes for first 20 seconds of program execution
+        if (currentTime - startTime < 15):
+            if (board.sonar_read(triggerPin)[0] > max_distance):
+                max_distance = board.sonar_read(triggerPin)[0]
+            currentTime = time.time()
 
-get_distance(display)
+        elif ((max_distance - board.sonar_read(triggerPin)[0] >= 25) and emailNotif == True):
+            print("MOVEMENT DETECTED. ALERT SENT.")
+            mail_protocol.alert_email()
+            emailNotif = False
+            startTime = time.time()
+            currentTime = time.time()
+        
+        elif (emailNotif == False):
+            if (currentTime - startTime < 25):
+                emailNotif = true
+            currentTime = time.time()
+            
+print("The pymata4 library can be found at https://github.com/MrYsLab/pymata4 \nThe following code was developed by Nihal Sahu and Nishan Sivakumar\n")
+
+get_distance()
 
